659b67a19594bbdbb7f330d0728a9a10f3885228
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-11 12:42:46 +0200

    added space

--- helloworld.php
+++ helloworld.php
@@ -1,9 +1,9 @@
 <?php
 //Variable starts with $' string has to use quoatation marks
    $first_name = "Hakan";
-   $last_name = " Pelit";
+   $last_name = "Pelit";
    //for adding up numbers + sign
 //show code to browser
-   echo $first_name.$last_name;
+   echo $first_name." ".$last_name;
   
 ?>
