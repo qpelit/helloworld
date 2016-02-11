7a756b0b912e224a2631d4a61e73537dec2e8484
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-11 12:39:03 +0200

    added some codes

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,9 @@
-Hello World
-
-changed
-
-hakan
-
-hak
+<?php
+//Variable starts with $' string has to use quoatation marks
+   $first_name = "Hakan";
+   $last_name = " Pelit";
+   //for adding up numbers + sign
+//show code to browser
+   echo $first_name.$last_name;
+  
+?>
